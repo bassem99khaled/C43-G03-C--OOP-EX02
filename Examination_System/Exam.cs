d1cb6f66eb13d16c97bb0d9407101cfa8573b14c
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-15 18:47:03 +0200

    Exam Class

---
+++ Examination_System/Exam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G03_C__OOP_EX02.Examination_System
+{
+    public abstract class Exam
+    {
+        #region Property
+        public int TimeOfExam { get; set; }
+        public int NumberOfQuestions { get; set; }
+
+        #endregion
+
+        #region Method
+        public abstract void ShowExam();
+        #endregion
+
+    }
+}
