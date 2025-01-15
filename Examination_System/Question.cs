fb5212e532474cb9680299cf624e271fb7b10076
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-15 18:43:03 +0200

    Question Class

--- Examination_System/Question.cs
+++ Examination_System/Question.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace C43_G03_C__OOP_EX02.Examination_System
 {
-    internal class Question
+    public class Question
     {
+        #region Property
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+        public Answer[] AnswerList { get; set; }
+        public int RightAnswer { get; set; }
+
+        public List<int> ChoiceUser { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public Question()
+        {
+            Header = string.Empty;
+            Body = string.Empty;
+            Mark = 0;
+            AnswerList = new Answer[0]; 
+        }
+        #endregion
     }
 }
