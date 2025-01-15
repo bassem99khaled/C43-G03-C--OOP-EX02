d64945b0e793e2a912792dfc1c3f5a556188caf7
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-15 18:50:09 +0200

    MCQQuestion Class

---
+++ Examination_System/MCQQuestion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G03_C__OOP_EX02.Examination_System
+{
+    internal class MCQQuestion : Question
+    {
+
+        #region Constructor
+        public MCQQuestion()
+            {
+                AnswerList = new Answer[4]; 
+                for (int i = 0; i < AnswerList.Length; i++)
+                {
+                    AnswerList[i] = new Answer();
+                }
+            }
+        #endregion
+
+    }
+}
